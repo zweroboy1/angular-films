f2af6632c3ee8cfef4aa5f9db5d42c77aecd3c6f
Oleksii Roman <fprognoz@gmail.com>
2026-05-04 01:26:58 +0300

    feat: add film detail page

--- src/app/pages/film-detail/film-detail.ts
+++ src/app/pages/film-detail/film-detail.ts
@@ -1,25 +1,29 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import { Component, computed, effect, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { BreadcrumbsService } from '../../core/breadcrumbs.service';
 import { FilmsService } from '../../core/films.service';
 import { DurationPipe } from '../../shared/pipes/duration.pipe';
 
 @Component({
   selector: 'app-film-detail',
-  imports: [DurationPipe],
+  imports: [DurationPipe, RouterLink],
   templateUrl: './film-detail.html',
+  styleUrl: './film-detail.scss',
 })
-export class FilmDetailComponent implements OnInit {
+export class FilmDetailComponent {
   private readonly breadcrumbs = inject(BreadcrumbsService);
-  private readonly filmsService = inject(FilmsService);
+  protected readonly filmsService = inject(FilmsService);
 
   readonly id = input<string>('');
 
   readonly film = computed(() => this.filmsService.getById(Number(this.id())));
 
-  ngOnInit(): void {
-    this.breadcrumbs.set([
-      { label: 'Home', url: '/' },
-      { label: this.film()?.title ?? 'Film #' + this.id() },
-    ]);
+  constructor() {
+    effect(() => {
+      this.breadcrumbs.set([
+        { label: 'Home', url: '/' },
+        { label: this.film()?.title ?? 'Film #' + this.id() },
+      ]);
+    });
   }
 }
