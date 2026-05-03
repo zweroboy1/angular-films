d5aa009137670274b86c87f0a35deb54e6446ebd
Oleksii Roman <fprognoz@gmail.com>
2026-05-04 02:56:47 +0300

    feat: update about page

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -4,6 +4,7 @@ import { BreadcrumbsService } from '../../core/breadcrumbs.service';
 @Component({
   selector: 'app-about',
   templateUrl: './about.html',
+  styleUrl: './about.scss',
 })
 export class AboutComponent implements OnInit {
   private readonly breadcrumbs = inject(BreadcrumbsService);
